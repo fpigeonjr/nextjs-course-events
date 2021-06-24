6353852bc45fb571ad69c49c3272b229751f8792
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-06-23 20:47:34 -0500

    adds routes programatically

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
 import { Fragment } from 'react'
+import { useRouter } from 'next/router'
 import { getAllEvents } from '../../dummy-data'
 import EventList from '../../components/events/event-list'
 import EventsSearch from './events-search'
 
 function AllEventsPage() {
+  const router = useRouter()
   const events = getAllEvents()
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
+
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   )
