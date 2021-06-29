3748c450ae7467f08c2db0f049789d2a7aeee0e4
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-06-29 06:19:08 -0500

    adds filter

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,8 @@
+import { Fragment } from 'react'
 import { useRouter } from 'next/router'
 import { getFilteredEvents } from '../../dummy-data'
 import EventList from '../../components/events/event-list'
+import ResultsTitle from '../../components/events/results-title'
 
 function FilteredEventsPage() {
   const router = useRouter()
@@ -35,10 +37,14 @@ function FilteredEventsPage() {
   if (!filteredEvents || filteredEvents.length === 0) {
     return <p>No events found.</p>
   }
+
+  const date = new Date(numYear, numMonth - 1)
+
   return (
-    <div>
+    <Fragment>
+      <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
-    </div>
+    </Fragment>
   )
 }
 
